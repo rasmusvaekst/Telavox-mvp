aa32e5d6ce915b42024d8f59eee22a8583717e29
“rasmusvaekst” <rasmus.trap@vaekstgroup.dk>
2023-08-28 16:04:55 +0200

    call

--- telavox/app.js
+++ telavox/app.js
@@ -6,41 +6,67 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const username = 'test';
-const password = 'test';
+app.use((req, res, next) => {
+  req.jwtToken = process.env.API_WEBTOKEN;
+  next();
+});
 
-async function getAuthToken() {
+
+  app.get('/calls', async (req, res) => {
     try {
-      const response = await axios.post('https://api.telavox.se/auth/token', {
-        username: process.env.TELAVOX_USERNAME,
-        password: process.env.TELAVOX_PASSWORD
+      const authToken = process.env.API_WEBTOKEN;
+  
+      const response = await axios.get('https://api.telavox.se/calls', {
+        headers: {
+          Authorization: `Bearer ${authToken}`
+        }
       });
   
-      return response.data.token;
+      res.json(response.data);
     } catch (error) {
-      throw new Error('Failed to obtain JWT token');
+      console.error('Error making API request:', error.message);
+      res.status(500).json({ error: 'An error occurred while making the API request' });
     }
-  }
+  });
 
-app.get('/calls', async (req, res) => {
+  
+  // Endpoint to start a call
+app.get('/start-call/:number', async (req, res) => {
   try {
-    const authToken = await getAuthToken();
+    const number = req.params.number;
+    const autoanswer = true;
 
-
-    const response = await axios.get('https://api.telavox.se/calls', {
+    const response = await axios.get(`https://api.telavox.se/dial/${number}`, {
       headers: {
-        Authorization: `Bearer ${authToken}`
+        Authorization: `Bearer ${req.jwtToken}`
+      },
+      params: {
+        autoanswer: autoanswer
       }
     });
 
     res.json(response.data);
   } catch (error) {
-    console.error('Error making API request:', error.message);
-    res.status(500).json({ error: 'An error occurred while making the API request' });
+    console.error('Error starting call:', error.message);
+    res.status(500).json({ error: 'An error occurred while starting the call' });
   }
 });
 
+// Endpoint to hang up a call
+app.get('/hangup', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.telavox.se/hangup', {
+      headers: {
+        Authorization: `Bearer ${req.jwtToken}`
+      }
+    });
 
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error hanging up call:', error.message);
+    res.status(500).json({ error: 'An error occurred while hanging up the call' });
+  }
+});
 
 
 app.listen(PORT, () => {
